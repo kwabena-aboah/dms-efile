5c00b800b6477489880c084706eeb0b3fb557c73
Mintah Obed Aboah <obed30mintah@yahoo.com>
2020-01-02 09:46:49 -0500

    update settings

--- efile/settings.py
+++ efile/settings.py
@@ -37,11 +37,12 @@ MANAGERS = ADMINS
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'mintahkwabena13@gmail.com'
-EMAIL_HOST_PASSWORD = r'kwabena13'
+EMAIL_HOST_USER = '' # your email address here
+EMAIL_HOST_PASSWORD = r'' # your password goes here
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = 'Admin<admin@127.0.0.1:8000>'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
